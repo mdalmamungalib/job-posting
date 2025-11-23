8aa9d874d0b79bafb9cf4afb1eb74817ba024c94
MD Al Mamun Galib <mdalmamungalib3929@gmail.com>
2025-11-23 21:41:04 +0600

    some folder add

---
+++ app/(backend)/layout.js
@@ -0,0 +1,9 @@
+const layout = ({Children}) => {
+    return (
+        <div>
+            {Children}
+        </div>
+    );
+};
+
+export default layout;
---
+++ app/(frontend)/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            This is working from the frontend side.
+        </div>
+    );
+};
+
+export default page;
