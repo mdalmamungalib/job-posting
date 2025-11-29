f044f9b93baaf9ee37e068b95665b582b3605f4f
MD Al Mamun Galib <108384550+mdalmamungalib@users.noreply.github.com>
2025-11-29 12:29:53 +0600

    signup page add

--- app/(front-end)/signUp/page.jsx
+++ app/(front-end)/signUp/page.jsx
@@ -1,16 +1,20 @@
 "use client";
-import Head from 'next/head';
-import SignUpPage from '@/components/Frontend/SignUpPage/SignUpPage';
+import Head from "next/head";
+import SignUpPage from "@/components/Frontend/SignUpPage/SignUpPage";
 
-export default function Signup() {
+const page = () => {
   return (
     <>
       <Head>
         <title>Sign Up - Job Posting Platform</title>
-        <meta name="description" content="Create your account to start applying for jobs" />
+        <meta
+          name="description"
+          content="Create your account to start applying for jobs"
+        />
       </Head>
-      <SignUpPage
-      />
+      <SignUpPage />
     </>
-  );    
-}
+  );
+};
+
+export default page;
