ec0f1a4c43914b44c9570a9e521a19855d99a32b
MD Al Mamun Galib <mdalmamungalib3929@gmail.com>
2025-11-30 21:06:11 +0600

    some update

---
+++ app/(front-end)/signup/page.jsx
@@ -0,0 +1,20 @@
+"use client";
+import Head from "next/head";
+import SignUpPage from "@/components/Frontend/SignUpPage/SignUpPage";
+
+const page = () => {
+  return (
+    <>
+      <Head>
+        <title className="">Sign Up - Job Posting Platform</title>
+        <meta
+          name="description"
+          content="Create your account to start applying for jobs"
+        />
+      </Head>
+      <SignUpPage />
+    </>
+  );
+};
+
+export default page;
